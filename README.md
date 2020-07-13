1257aea206f8b615c56dce3837ef3eb6d8205fb2
Tonya Sims <tonyasims@Tonyas-MacBook-Pro.local>
2020-07-13 11:20:27 -0500

    Changed README and added method to contact_book.py

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Contact Book
 
 A contact book is a tool for saving a contact’s details, such as name, address, phone number, and email address. With this contact book project, you can use it to save and find contact details.
 
-Users can save their contacts to a file. It’ll always be accessible from their computer, through the command-line.
+Users can save their contacts to a database. It’ll always be accessible from their computer, through the command-line.
 
 Technical Details
 
@@ -10,4 +10,4 @@ The main objective of this project is to save contact details. You can use comma
 
 Some features that are implemented include the ability to add new contacts as well as commands to delete contacts, update contact information, and list saved contacts. You can also allow users to list contacts using different parameters, such as alphabetical order or contact creation date.
 
-Since it’s a command-line project, the contacts will save to a file on your local computer. 
+Since it’s a command-line project, the contacts will save to a database on your local computer. 
---
+++ contact_book.py
@@ -0,0 +1,128 @@
+
+import argparse
+from datetime import date
+import sqlite3
+from sqlite3 import Error
+
+class ContactBook:
+    conn = sqlite3.connect('contact.db')
+    c = conn.cursor()
+
+    c.execute(""" CREATE TABLE IF NOT EXISTS contacts (
+                            firstname text,
+                            lastname text,
+                            email text,
+                            phone text
+                ) """) 
+
+
+
+    def __init__(self, first_name=None, last_name=None, email=None, phone=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone = phone
+        self.date_created = date.today()
+
+
+    
+    def get_args(self):
+        parser = argparse.ArgumentParser(description="Creating a contact book.")
+        parser.add_argument('--firstname', type=str, help='The first name of your contact')
+        parser.add_argument('--lastname', type=str, help='The last name of your contact')
+        parser.add_argument('--email', type=str, help="The email address of your contact")
+        parser.add_argument('--phone', type=str, help="The phone number of your contact")
+        parser.add_argument('--update', help="This will allow you to update a contact", action="store_true")
+        parser.add_argument('--all', help="Will list all of your contacts", action="store_true")
+        parser.add_argument('--deleteall', help="Will delete all your contacts", action="store_true")
+        args = parser.parse_args()
+       
+
+        print(f"this is args: {args}")
+        return args
+
+    def insert_contact(self):
+        # insert into 24:05
+        # only insert if name, last, email and phone number are not the same as existing contact
+        contact_args = contact.get_args()
+        print(f"this is contact_args: {contact_args}")
+        name_list = self.c.execute("SELECT * FROM contacts WHERE firstname=:firstname AND lastname=:lastname", {'firstname':contact_args.firstname, 'lastname':contact_args.lastname})
+
+        if (contact_args.firstname, contact_args.lastname, contact_args.email, contact_args.phone) not in name_list and not contact_args.all: # check if tuple is in the list, if it's not then insert it into the table
+            print("...Not in table so insert...")
+            self.c.execute("INSERT INTO contacts VALUES (:firstname, :lastname, :email, :phone)", 
+                            {'firstname': contact_args.firstname, 'lastname': contact_args.lastname, 'email':contact_args.email, 'phone':contact_args.phone})
+            self.conn.commit()
+            self.conn.close()
+        else:
+            print("...Contact is already in your contact book...")
+
+
+    def update_contact(self):
+        # update 
+        field_changed = input("Which field do you want to update? Type 'firstname','lastname','email' or 'phone > ")
+        field_changed_to = input("What do you want to change the field to? > ")
+        firstname_value = input("What is the first name of the contact you want to change? > ")
+        lastname_value = input("What is the last name of the contact you want to change? > ")
+
+        self.c.execute("""UPDATE contacts SET field_changed = :field_changed_to
+                          WHERE firstname = :firstname_value and lastname = :lastname_value """,
+                          {'field_changed': field_changed_to, 'firstname': firstname_value, 'lastname': lastname_value})
+        
+        self.conn.commit()
+        self.conn.close()
+
+
+
+
+        
+
+    def delete_all_contacts(self):
+        # deletes all contacts from your contact book
+        confirmation = input("Are you sure you want to delete ALL your contacts? Type 'Yes' or 'No' > ")
+        if confirmation == "Yes" or  confirmation == "yes":
+            print("..You are deleting all your contacts...")
+            self.c.execute("DELETE FROM contacts")
+            self.conn.commit()
+            self.conn.close()
+        elif confirmation == "No" or confirmation == "no":
+            print("Phew, your contacts will not be deleted.")
+    
+
+    def list_contacts(self):
+        """
+        Selects all the contacts in the contact book and prints them out to the console
+        """
+
+        self.c.execute("SELECT * FROM contacts")
+        contacts = self.c.fetchall()
+        for item in contacts:
+            print(item)
+    
+
+    def list_contact_in_order(self):
+        pass
+
+    def list_contact_by_date(self):
+        pass
+
+    def main(self):
+
+        args = contact.get_args()
+
+        if args.all:
+            contact.list_contacts()
+        elif args.deleteall:
+            contact.delete_all_contacts()
+        elif args.firstname:
+            contact.insert_contact()
+        elif args.update:
+            contact.update_contact()
+
+
+
+contact = ContactBook()
+contact.main()
+
+  
+            
